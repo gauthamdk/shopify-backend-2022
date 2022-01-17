2384972299b3c0ed5f45ac9216b9cabfd2751652
Gautham <gauthamkumar.gk.gk@gmail.com>
2022-01-17 12:05:35 +0400

    get data from api

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,25 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import axios from "axios";
+
+import { IItem } from "./interfaces/Item";
 
 function App() {
+  const [items, setItems] = useState<IItem[]>([]);
+
+  useEffect(() => {
+    const getData = async () => {
+      const res = await axios.get("/api/display");
+      setItems(res.data);
+    };
+
+    getData();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="App">Front end</div>
+      {items.length > 0 ? console.log(items) : <></>}
+    </>
   );
 }
 
